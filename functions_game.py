d065b338a886ff5eda52430c11dbf07b23924108
nicos <nicolas.turck@gmail.com>
2019-11-08 10:38:08 +0100

    create boucle while for user choice and create a random choice program

--- functions_game.py
+++ functions_game.py
@@ -11,4 +11,10 @@ prog= (0)#create prog variable  = 0
 while user != 3 and prog != 3:#while user or programm not 3 points display the choice
     print("               1:stone\n               2:leaf\n               3:scissors\n")
     userAnswers = input("{} enter number of your choice:\n".format(name))#request to user enter a number of is choice
-    
+
+    while userAnswers != "1" and userAnswers != "2" and userAnswers != "3":#while user answer is not "1" "2" or "3" display enter a number
+        userAnswers = input("{} enter number of your choice:".format(name))
+        print("entry autorized number")
+
+    prg = random.randint(1, 3)#prog choice a number
+    print("program choice : {}\n".format(prg))#display program choice
