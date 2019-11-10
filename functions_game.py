3f8e0feae8d5c22d1b9dcfb00366f0aa791b6293
nicos <nicolas.turck@gmail.com>
2019-11-10 18:38:30 +0100

    change code shi fu mi create function and write in README.md

--- functions_game.py
+++ functions_game.py
@@ -1,92 +1,76 @@
-import random#import random method to prog choice
-def game():#create function game
+import random #import random method to prog choice
+
+def gamePlay(name, user, prog):
+    while user != 3 and prog != 3: #loop while  user or programm not 3 points display the choice
+            print("               1:stone\n               2:leaf\n               3:scissors\n") #displays numbers and items
+            userAnswers = input("{} enter number of your choice:\n".format(name)) #request to user enter a number of is choice
+
+            while userAnswers != "1" and userAnswers != "2" and userAnswers != "3":# loop until  user enters a valid choice
+                userAnswers = input("{} enter number of your choice:".format(name)) #request to user enter a number of his choice
+                print("entry autorized number")
+
+            prg = str(random.randint(1, 3)  )#prog choice a random number with the method random
+            print("program choice : {}\n".format(prg)) #display program choice
+
+            if userAnswers == prg or prg == userAnswers: #condition for answer user and program
+                print("{} choice {} program choice {}".format(name, userAnswers, prg)) #return the choice with method format
+                print(">>>>>>equality<<<<<<\n")
+                user= user+0 #no add points to user and program score
+                prog = prog+0
+
+            elif prg == "1" and userAnswers == "2": #conditions for user and program answer
+                print("{} choice {} program choice {}".format(name, userAnswers, prg)) #return the choice with method format
+                print("~~~~~~~~~~~~~{} you win~~~~~~~~~~~~\n".format(name))
+                user = user+1    #add pointsto user score
+
+            elif prg == "2" and userAnswers == "3": #conditions for user and program answer
+                print("{} choice {} program choice {}".format(name, userAnswers, prg)) #return the choice with method format
+                print("~~~~~~~~~~~~~{} you win~~~~~~~~~~~~\n".format(name))
+                user = user+1  #add points to user  score
+
+            elif prg == "3" and userAnswers == "1": #conditions for user and program answer
+                print("{} choice {} program choice {}".format(name, userAnswers, prg)) #return the choice with method format
+                print("~~~~~~~~~~~~~{} you win~~~~~~~~~~~~\n".format(name))
+                user = user+1 #add points to user score
+
+            else:    #conditions for user and program answer
+                print("{} choice {} program choice {}".format(name, userAnswers, prg)) #return the choice with method format
+                print("~~~~~~~~~~~~~ you loose~~~~~~~~~~~~\n")
+                prog = prog+1 #add points program score
+
+            print("{} your score : {}".format(name, user))#display name and score of user with method format
+            print("program score : {}\n".format(prog))#display score of program
+            print("**************************************")
+
+            calcScore(name, user, prog)
+    return user
+
+
+def nameUser(): #this function registered a name enter by player or assign a pseudo to player
     print("~(~)~(~)~(~)~(~)~(~)~(~)~(~)~(~)~")
-    name = input("enter your name :")#request to user enter is name
-    name = name.upper()#transformed user name in uppercase
-    if name == "":#if user no entru name
-        name = "TUX"#name choice TUX to username
+    name = input("enter your name :") #request to user enter is name
+    name = name.upper() #transformed user name in uppercase
+    if name == "": #if user press enter and no entry name
+        name = "TUX" #name choice TUX to username
+    return name #the function return name player for continue game and reuse name in game
 
-    score = {}#create dictionnary
-    user= 0#create variable score user = 0
-    prog= 0#create  variable score program  = 0
+def calcScore(name, user, prog): #this function calculated the score of user and program
+    score_all = {}
+    scorUser = print("your score {} and program score {}".format(user, prog)) #display score
+    if user == 3 : #if user score is equal 3 the function print win and add name and score in a dict 'score'
+        score_all.update({name : +1})
+        win = print("~~~~~~<<<<<<|**YOU WIN**|>>>~~~~~~~~~")
+        #print(score)
+        return win, scorUser, score_all #fonction return action print and dict score modify
 
-    while user != 3 and prog != 3:#while user or programm not 3 points display the choice
-        print("               1:stone\n               2:leaf\n               3:scissors\n")#displays numbers and items
-        userAnswers = input("{} enter number of your choice:\n".format(name))#request to user enter a number of is choice
+    elif prog == 3: #if program score is equal 3 the function print loose and add name winner in dict 'score
+        score_all.update({"prog" : +1})
+        loose = print("~~~~~~~~<<<<|**PROG WIN**|>>>>~~~~~~~")
+        #print(score)
+        return loose, score_all#fonction return action print and dict score modify
 
-
-
-        while userAnswers != "1" and userAnswers != "2" and userAnswers != "3":#loop until  user enters a valid choice
-            userAnswers = input("{} enter number of your choice:".format(name))
-            print("entry autorized number")
-
-        prg = random.randint(1, 3)#prog choice a number
-        print("program choice : {}\n".format(prg))#display program choice
-
-
-        if userAnswers == "1" and prg == 2:
-            print("your choice STONE and program choice LEAF ")
-            print("~~~~~~~~~~~~~{} you loose~~~~~~~~~~~~\n".format(name))
-            #condition of win or loose
-            prog =  prog+1#add point
-
-
-
-
-        if userAnswers == "2" and prg == 3:
-            print("your choice LEAF and program choice SCISSORS")
-            print("~~~~~~~~~~~~~{} you loose~~~~~~~~~~~~\n".format(name))
-            prog =  prog+1#add point
-            #condition of win or loose
-
-
-
-        if userAnswers == "3" and prg == 1:
-            print("your choice SCISSORS and program choice STONE")
-            print("~~~~~~~~~~~~~{} you loose~~~~~~~~~~~~\n".format(name))
-            prog =  prog+1#add point
-            #condition of win or loose
-
-
-        if prg == 1 and userAnswers == "2":
-            print("your choice LEAF and program choice STONE")
-            print("~~~~~~~~~~~~~{} you win~~~~~~~~~~~~\n".format(name))
-            user = prog+1#add point
-            #condition of win or loose
-
-
-        if prg == 2 and userAnswers == "3":
-            print("your choice SCISSORS and program choice LEAF")
-            print("~~~~~~~~~~~~~{} you win~~~~~~~~~~~~\n".format(name))
-            user = user+1#add point
-            #condition of win or loose
-
-
-        if prg == 3 and userAnswers == "1":
-            print("your choice STONE and program choice SCISSORS")
-            print("~~~~~~~~~~~~~{} you win~~~~~~~~~~~~\n".format(name))
-            #condition of win or loose
-            user = user+1#add point
-
-        if userAnswers == prg:
-            print(">>>>>>equality<<<<<<\n")
-
-    print("{} your score : {}".format(name, user))#display score of user
-    print("program score : {}\n".format(prog))#display score of program
-    print("**************************************")
-    if user == 3 :#condition if user win
-        print("            |***********|")
-        print("~~~~~~<<<<<<|**YOU WIN**|>>>~~~~~~~~~")
-        print("            |***********|")
-        print("your score {} and program score {}".format(user, prog))
-        score.update({name : 1})#add score to dict score
-        print(score)#print dict
-
-
-    if prog == 3:#condition if program win
-        print("            |************|")
-        print("~~~~~~~~<<<<|**PROG WIN**|>>>>~~~~~~~")
-        print("            |************|")
-        print("your score {} and program score {}".format(user, prog))
-        score.update({"bots" : 1})#add score to dict score
-        print(score)#display score
+def helpGame(): #this function display the rules of the game
+    print("~~~~~~~~~~RULES OF THE GAME~~~~~~~~~~\n")#display help game
+    print("     its game is very easy \n     you play versus the program \n     enter number of items you choice")
+    print("     the stone wins scissors, the leaf wins the stones \n     and the scissor wins the leaf")
+    print("     the winners is the first with 3 points")
--- shi_fu_mi.py
+++ shi_fu_mi.py
@@ -1,40 +1,34 @@
-#import sys
-from functions_game import game
-
+from functions_game import *
+import random
 print("~~~~~~~~~~WELCOMME SHI FU MI~~~~~~~~~~\n")
+# i create many variable to reuse after
+user= (0)
+prog= (0)
 
-def userChoice():#create function menu
-    while 1:
-        print("\n~~~~~~~~~~~~~~~~MENU~~~~~~~~~~~~~~~~~\n\n                1:Play \n                2:Score \n                3:Help \n                4:Exit")
+name = nameUser() # i declae name equal the return of the function nameUser
 
-        userEntry = input("\nEnter number corresponding of your choice:\n")#request answer to user
+while 1: #loop for the choice of user
+    # i drisplay the menu of the game
+    print("\n~~~~~~~~~~~~~~~~MENU~~~~~~~~~~~~~~~~~\n\n                1:Play \n                2:Score \n                3:Help \n                4:Exit")
+    userEntry = input("\n{} Enter number corresponding of your choice:\n".format(name)) #request answer to user
 
-        if userEntry == None:#if user no entry answer
-            userChoice()#return to fonction userChoice
+    if userEntry != "1" and userEntry != "2" and userEntry != "3" and userEntry != "4": #condition if user enter is no valid
+        print("{} enter a number".format(name))#request enter a number
+
+    if userEntry == "1":#loop if user answer  entry is 1
+        print(">>>>>>>>>>>>START GAME<<<<<<<<<<<<<")
+        gamePlay(name, user, prog)
+         #function to calculate score of user and program and return score
+
+    if userEntry == "2": #condition to display score dict
+        print("sorry no score")
 
 
 
-        if userEntry != "1" and userEntry != "2" and userEntry != "3" and userEntry != "4":#if user is no valide
-            print("enter a number")#request enter a number
-            userChoice()#return to fonction userChoice
+    if userEntry == "3": #conditions if  user answer is 3 display the rules of the game
+        helpGame()
 
-        if userEntry == "1":#if user answer is 1
-            game()#go to functions_game
-
-
-        if userEntry == "2":#if user answer is 2
-            print("\n~~~~~~~~~~~~~~~~SCORE~~~~~~~~~~~~~~~~")#display all score
-            return userChoice()#return to function userChoice
-
-        if userEntry == "3":#if user answer is 3
-            print("~~~~~~~~~~RULES OF THE GAME~~~~~~~~~~\n")#display help game
-            print("     its game is very easy \n     you play versus the program \n     enter number of items you choice")
-            print("     the stone wins scissors, the leaf wins the stones \n     and the scissor wins the leaf")
-            print("     the winners is the first with 3 points")
-            return userChoice()#return to function_game
-
-        if userEntry == "4":#if user answer is 4 display good bye and quit the game
-            print("\n~~~~~~~~~~~~~~GOOD BYE~~~~~~~~~~~~~")
-            exit()
-
-userChoice()
+    if userEntry == "4":#if user answer is 4 display good bye and quit the game
+        print("~~~~~~~~~~~~~~~~{}~~~~~~~~~~~~~~~~~~".format(name))
+        print("\n~~~~~~~~~~~~~~GOOD BYE~~~~~~~~~~~~~")
+        exit() #quit the program
